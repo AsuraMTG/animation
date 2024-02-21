865d2c66a3f18830d58d499b86fb36afbdc42a22
masmar575 <masmar575@102PC08>
2024-02-21 11:49:33 +0100

    Add project files.

---
+++ animation/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿
+namespace animation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::animation.Properties.Resources.R;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(600, 600);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(618, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Indít";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(619, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Fuchsia;
+            this.imageList1.Images.SetKeyName(0, "nurseBlack_walkA_0000.bmp");
+            this.imageList1.Images.SetKeyName(1, "nurseBlack_walkA_0001.bmp");
+            this.imageList1.Images.SetKeyName(2, "nurseBlack_walkA_0002.bmp");
+            this.imageList1.Images.SetKeyName(3, "nurseBlack_walkA_0003.bmp");
+            this.imageList1.Images.SetKeyName(4, "nurseBlack_walkA_0004.bmp");
+            this.imageList1.Images.SetKeyName(5, "nurseBlack_walkA_0005.bmp");
+            this.imageList1.Images.SetKeyName(6, "nurseBlack_walkA_0006.bmp");
+            this.imageList1.Images.SetKeyName(7, "nurseBlack_walkA_0007.bmp");
+            this.imageList1.Images.SetKeyName(8, "nurseBlack_walkA_0008.bmp");
+            this.imageList1.Images.SetKeyName(9, "nurseBlack_walkA_0009.bmp");
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 40;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(700, 11);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1477, 839);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ animation/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace animation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        bool indul = false;
+        int szamlalo = 0;
+        Bitmap hatter;
+        Bitmap minta;
+        Bitmap kicsi;
+        Graphics g;
+        int aktx = 14;
+        
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (indul == false)
+            {
+                indul = true;
+                button1.Text = "Állj";
+                timer1.Enabled = true;
+                szamlalo++;
+            }
+            else
+            {
+                indul = false;
+                button1.Text = "Indít";
+                timer1.Enabled = false;
+                
+            }
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if ((indul == true) && (szamlalo < 20))
+            { 
+                szamlalo++;
+                label1.Text = Convert.ToString(szamlalo);
+            }
+            else
+            {
+                indul = false;
+                if ((indul == false) && (szamlalo > 0))
+                {
+                    szamlalo--;
+                    label1.Text = Convert.ToString(szamlalo);
+                }
+                if (szamlalo == 0)
+                {
+                    indul = true;
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string utvonal = openFileDialog1.FileName;
+                hatter = (Bitmap)Image.FromFile(utvonal);
+                minta = (Bitmap)Image.FromFile(utvonal);
+                
+            }
+            pictureBox1.Image = hatter;
+            imageList1.TransparentColor = Color.Fuchsia;
+            kicsi = (Bitmap)imageList1.Images[0];
+            g = Graphics.FromImage(hatter);
+            g.DrawImage(kicsi, aktx, 200, 44, 44); 
+        }
+    }
+}
